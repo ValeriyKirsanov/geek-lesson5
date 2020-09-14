1c06d2c74314e6d47b0ebcc09479214af45a3d72
ValeriyKirsanov <70278649+ValeriyKirsanov@users.noreply.github.com>
2020-09-14 20:45:59 +0300

    Update exemp13.py

--- exemp13.py
+++ exemp13.py
@@ -1,4 +1,7 @@
 # Создать программно файл в текстовом формате,
 # записать в него построчно данные, вводимые пользователем.
+exemp = input("input text")
 out_f = open("text.txt", "w")
+out_f.write(exemp)
+
 out_f.close()
